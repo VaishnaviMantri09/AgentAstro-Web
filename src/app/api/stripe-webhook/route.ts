03fa2db2d0928f173c1180fe42f3314c3e5d007b
Vaishnavi Mantri <155553314+VaishnaviMantri09@users.noreply.github.com>
2025-08-07 13:48:46 -0400

    Updated route.ts

--- src/app/api/stripe-webhook/route.ts
+++ src/app/api/stripe-webhook/route.ts
@@ -4,7 +4,7 @@ import Stripe from "stripe";
 
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2024-08-01",
+  apiVersion: "2025-07-30.basil",
 });
 
 const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET!;
