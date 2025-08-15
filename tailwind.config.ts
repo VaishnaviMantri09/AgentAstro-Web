8b8213b9ffb0db6f68d5b334f217afa27838458b
Vaishnavi Mantri <155553314+VaishnaviMantri09@users.noreply.github.com>
2025-08-14 23:31:05 -0400

    Updated tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
     },
     extend: {
       fontFamily: {
-        switzer: ["Switzer"],
+         switzer: ["Switzer", "sans-serif"],
       },
       container: {
         center: true,
