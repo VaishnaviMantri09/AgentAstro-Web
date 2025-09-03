ee7b1bde6b9e0999aae8b1f13ccf662d433557c2
Vaishnavi Mantri <155553314+VaishnaviMantri09@users.noreply.github.com>
2025-09-03 13:03:42 -0400

    Update route.ts

--- src/app/api/checkout-session/route.ts
+++ src/app/api/checkout-session/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-    apiVersion: "2025-07-30.basil",
+    apiVersion: "2025-08-27.basil",
 });
 
 export async function GET(request: NextRequest) {
@@ -24,3 +24,4 @@ export async function GET(request: NextRequest) {
         return NextResponse.json({ error: err.message }, { status: 500 });
     }
 }
+
