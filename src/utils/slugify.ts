b1aa9bf7d0a40b53fa05196eb2978ec63c364770
Vaishnavi Mantri <155553314+VaishnaviMantri09@users.noreply.github.com>
2025-09-21 01:12:06 -0400

    Deleted src directory

--- src/utils/slugify.ts
+++
@@ -1,9 +0,0 @@
-export function slugify(text: string): string {
-    return text
-        .toLowerCase()
-        .replace(/[’'.]/g, '')
-        .replace(/[^a-z0-9\s-]/g, '')
-        .trim()
-        .replace(/\s+/g, '-')
-        .replace(/-+/g, '-');
-}
