a34ff3ad7c9f4562da87467c95ab31b7ff0db2de
Vaishnavi Mantri <155553314+VaishnaviMantri09@users.noreply.github.com>
2025-10-24 04:07:41 -0400

    Updated new AgentAstro layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,19 +7,19 @@ import logoIcon from '@/../public/logo.png';
 const dmSans = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "AgentAstro - Streamline Medical Device Approvals with AI",
-  description: "AgentAstro accelerates regulatory approval for medical devices through AI, ensuring compliance and data security.",
+  title: "Agent Astro - Streamline Medical Device Approvals with AI",
+  description: "Agent Astro accelerates regulatory approval for medical devices through AI, ensuring compliance and data security.",
   openGraph: {
-    title: "AgentAstro - Streamline Medical Device Approvals with AI",
-    description: "AgentAstro accelerates regulatory approval for medical devices through AI, ensuring compliance and data security.",
+    title: "Agent Astro - Streamline Medical Device Approvals with AI",
+    description: "Agent Astro accelerates regulatory approval for medical devices through AI, ensuring compliance and data security.",
     type: "website",
-    url: "https://www.agentastro.ai/",
+    url: "https://www.AgentAstro.ai/",
     images: [
       {
         url: logoIcon.src,
         width: 1200,
         height: 630,
-        alt: "AgentAstro Logo",
+        alt: "Agent Astro Logo",
       },
     ],
   },
